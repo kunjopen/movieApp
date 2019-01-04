164e63aac9d8b1fc2e36e7e864ba803776926299
KetanPatel <kakdiyaketan@gmail.com>
2019-01-04 14:04:09 +0530

    Create project
    Create project and folder structure.

---
+++ MovieApp/MovieApp/Controller/MovieListVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ketan on 1/3/19.
+//  Copyright © 2019 Ketan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
